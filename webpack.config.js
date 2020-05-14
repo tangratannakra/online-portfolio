3db13f87438c0a60d1d59e56e4594336fda24450
tangratannakra <stela.tasheva@gmail.com>
2020-05-14 09:52:52 +0300

    change webpack.config. creating bundeledjs.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
-{
-    entry: './app/assets/scripts/App.js'
+const path = require('path');
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundeled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',
+    watch: true
 }
