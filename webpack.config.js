8e61d46144f30ccfa3530f31af4905cdb7975251
tangratannakra <stela.tasheva@gmail.com>
2020-05-14 20:51:46 +0300

    added postcss and sass-loader, autoprefix dependencies. integrated the main style.scss into the App.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 const path = require('path');
+var urix = require("urix"); 
+
+//plugin stack for postCSS
+const postCssPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
 
 module.exports = {
     entry: './app/assets/scripts/App.js',
@@ -6,6 +14,30 @@ module.exports = {
         filename: 'bundeled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000
+    },
     mode: 'development',
-    watch: true
+    watch: true,
+    module: {
+       rules: [
+                {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                    //postCss
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: postCssPlugins
+                        }
+                    }
+                  ]
+                }
+            ]   
+            }
 }
