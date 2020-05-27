44f89e1d133097f27965bd65bd1aa4fa921e3375
tangratannakra <stela.tasheva@gmail.com>
2020-05-27 17:04:54 +0300

    appending github project element and adding html to the project + data.properties from github

--- src/scripts/githubprojects.js
+++ src/scripts/githubprojects.js
@@ -1,5 +1,23 @@
+const gitContainer = document.getElementById('github-projects');
+
+
 let buildProject = (data) => {
     console.log(data);
+    const projectContainer = document.createElement('div');
+    projectContainer.className = 'github';
+    gitContainer.appendChild(projectContainer);
+    projectContainer.innerHTML = `
+        <div class="github__title">
+            <h1>${data.name}</h1>
+        </div>
+        <div class="github__thumb"><img
+            src="https://banner2.cleanpng.com/20180214/tow/kisspng-organization-child-enterprise-resource-planning-ma-square-shape-cliparts-5a849f82ab0459.4031530415186410267005.jpg"
+            alt="" />
+        </div>
+        <div class="github--description">
+            <p>${data.description}</p>
+        </div>
+        `;
 };
 
 export default buildProject;
