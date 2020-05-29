27523b7552aff39e45ce6c675c7f0cae92bb97e1
tangratannakra <stela.tasheva@gmail.com>
2020-05-29 17:33:58 +0300

    filling certificates data and stylig

--- src/scripts/certificates/certificatEntry.js
+++ src/scripts/certificates/certificatEntry.js
@@ -1,23 +1,26 @@
 class CertificateEntry {
-    constructor(data) {
-        return this.buildCertEntry(data);
-    }
+  constructor(data) {
+    return this.buildCertEntry(data);
+  }
 
-    buildCertEntry(data) {
-        const certEntry = document.createElement('div');
-        certEntry.className = 'certificates-entry';
+  buildCertEntry(data) {
+    const certEntry = document.createElement('div');
+    certEntry.className = 'certificates-entry';
 
-        certEntry.innerHTML = `
+    certEntry.innerHTML = `
     <div class="certificates-entry__thumb">
-      <h1>${data.title}</h1>
+      <img src="${data.img}" alt="certificate image"/>
     </div>
-    <div class="certificates-entry__date">${data.date}</div>
-    <div class="cv-entry__description">
-      <p>d${data.description}</p>
-    </div>
+    <div class="certificates-entry__title"><h1><a href="${data.url}" target="_blank">${data.title}</a></h1></div>
     `;
-        return certEntry;
-    }
+    return certEntry;
+  }
 }
 
 export default CertificateEntry;
+
+{
+  /* <div class="certificates-entry__description">
+        <p>d${data.description}</p>
+      </div> */
+}
--- src/scripts/certificates/certificatesData.js
+++ src/scripts/certificates/certificatesData.js
@@ -1,10 +1,17 @@
 const certificates = [{
-        title: 'Title',
-        url: 'url'
+        img: '../src/img/NewUlogo-large-1.png',
+        url: 'url',
+        title: 'B.Sc. Computer Science and Communications Engineering'
     },
     {
-        title: 'Title',
-        url: 'url'
+        img: '../src/img/NewUlogo-large-1.png',
+        url: 'https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/',
+        title: 'JavaScript - The Complete Guide 2020 (Beginner + Advanced)'
+    },
+    {
+        img: '../src/img/NewUlogo-large-1.png',
+        url: 'url',
+        title: 'JavaScript: Understanding the Weird Parts'
     }
 ];
 
