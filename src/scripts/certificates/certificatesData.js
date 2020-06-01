c41b0e02b26fb62acad1ac993447c57ed6254faf
tangratannakra <stela.tasheva@gmail.com>
2020-06-01 16:59:21 +0300

    adding icons to src imgs folder + linkedin and github icon

--- src/scripts/certificates/certificatesData.js
+++ src/scripts/certificates/certificatesData.js
@@ -1,15 +1,15 @@
 const certificates = [{
-        img: '../src/img/ude-logo.png',
+        img: '../src/img/icons/ude-logo.png',
         url: 'https://www.uni-due.de',
         title: 'B.Sc. Computer Science and Communications Engineering'
     },
     {
-        img: '../src/img/NewUlogo-large-1.png',
+        img: '../src/img/icons/NewUlogo-large-1.png',
         url: 'https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced',
         title: 'JavaScript - The Complete Guide 2020 (Beginner + Advanced)'
     },
     {
-        img: '../src/img/NewUlogo-large-1.png',
+        img: '../src/img/icons/NewUlogo-large-1.png',
         url: 'url',
         title: 'JavaScript: Understanding the Weird Parts'
     }
