1394b5e9cf4e989a2682da9cf82b96e0a30cc3ca
tangratannakra <stela.tasheva@gmail.com>
2020-06-03 14:26:11 +0300

    changin relative path to src folder

--- src/scripts/recentProjects/recentPrData.js
+++ src/scripts/recentProjects/recentPrData.js
@@ -1,29 +1,29 @@
 const recentPrData = [{
-        thumb: './src/img/prjcts/interlabor.png',
+        thumb: '../src/img/prjcts/interlabor.png',
         title: 'Interlabor',
         description: 'Custom CSS / Custom JavaScript / Site Admin',
         url: 'http://www.inlab-bg.com/'
     },
     {
-        thumb: './src/img/prjcts/chervenataStena.png',
+        thumb: '../src/img/prjcts/chervenataStena.png',
         title: 'Chervenata Stena Park',
         description: 'UX/UI / WorpdPress / Custom CSS',
         url: 'http://chervenatastena.com/'
     },
     {
-        thumb: './src/img/prjcts/swissboutique.png',
+        thumb: '../src/img/prjcts/swissboutique.png',
         title: 'Swiss Boutique',
         description: 'JS Customization / Custom CSS / Site Admin',
         url: 'https://swissboutique.bg/'
     },
     {
-        thumb: './src/img/prjcts/phototravel.png',
+        thumb: '../src/img/prjcts/phototravel.png',
         title: 'PhotoTravel',
         description: 'WordPress and Custom CSS',
         url: 'http://phototravel.bg'
     },
     {
-        thumb: './src/img/prjcts/realestate.png',
+        thumb: '../src/img/prjcts/realestate.png',
         title: 'RealEstate Agency',
         description: 'CSS / JS / Single Page',
         url: '#'
