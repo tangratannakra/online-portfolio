c67460ec5cfae30ef6e71034beb705b636463aec
tangratannakra <stela.tasheva@gmail.com>
2020-06-05 18:03:22 +0300

    adding backround to above fold and styling

--- src/scripts/redShift.js
+++ src/scripts/redShift.js
@@ -1,116 +1,112 @@
-import gsap from 'gsap';
-
-const tl = gsap.timeline(
-    //     {
-    //     repeat: 5,
-    //     repeatDelay: 1
-    // }
-);
+import gsap from 'gsap/all';
 
 
-tl.set("#animation-text-bottom", {
-    opacity: 0.5,
-    duration: 5,
-    ease: 'back'
-});
+document.addEventListener('DOMContentLoaded', timeLineHandler, false);
 
 
-tl.set('.animation-wrapper', {
-        scale: 1.3,
-        ease: "easeOut"
-    })
-    .to('.animation-wrapper', {
-        scale: 1,
-        duration: 10,
-        ease: "easeOut"
-    }, 0);
+function timeLineHandler() {
+    const tl = gsap.timeline();
+
+    tl.set("#animation-text-bottom", {
+        opacity: 0.5,
+        duration: 5,
+        ease: 'back'
+    });
 
 
-tl.set('.animation-wrapper', {
-        opacity: 0.2
-    }, 0)
-    .to('.animation-wrapper', {
-        opacity: 1
-    }, 1)
-    .set('.animation-wrapper', {
-        x: -40,
-        y: 30
-    }, 2)
-    .set('.animation-wrapper', {
-        x: 0,
-        y: 0
-    }, 3);
+    tl.set('.animation-wrapper', {
+            scale: 0.8
+        })
+        .to('.animation-wrapper', {
+            scale: 1,
+            duration: 5,
+            ease: "back.inOut"
+        }, 0);
 
 
-tl.set('#F', {
-        x: -100,
-        y: 50
-    }, 3.9)
-    .set('#F', {
-        x: 0,
-        y: 0
-    }, 4.1);
+    tl.set('.animation-wrapper', {
+            opacity: 0.2
+        }, 0)
+        .to('.animation-wrapper', {
+            opacity: 1
+        }, 1);
 
-tl.set('#R', {
-        x: 300,
-        y: -150,
-        scale: 2
-    }, 5.25)
-    .set('#R', {
-        x: 0,
-        y: 0,
-        scale: 1
-    }, 5.5);
 
-tl.set('#O', {
-        x: -150,
-        y: 75
-    }, 4.75)
-    .set('#O', {
-        x: 0,
-        y: 0
-    }, 5.1)
-    .set('#O', {
-        x: 50,
-        y: 150
-    }, 5.4)
-    .set('#O', {
-        x: 0,
-        y: 0
-    }, 5.6);
+    tl.set('#F', {
+            x: -100,
+            y: 50
+        }, 3.9)
+        .set('#F', {
+            x: 0,
+            y: 0
+        }, 4.1);
 
-tl.set('.N', {
-        x: 0,
-        y: -15
-    }, 4.85)
-    .set('.N', {
-        x: 0,
-        y: 0
-    }, 5.2)
-    .set('.N', {
-        rotate: 180
-    }, 5.4)
-    .set('.N', {
-        rotate: 90
-    }, 5.6)
-    .set('.N', {
-        rotate: 0
-    }, 5.8);
+    tl.set('#R', {
+            x: 300,
+            y: -150,
+            scale: 2
+        }, 5.25)
+        .set('#R', {
+            x: 0,
+            y: 0,
+            scale: 1
+        }, 5.5);
 
-tl.set('#E', {
-        x: -300,
-        y: -120
-    }, 5.5)
-    .set('#E', {
-        x: 0,
-        y: 0
-    }, 5.75);
+    tl.set('#O', {
+            x: -150,
+            y: 75
+        }, 4.75)
+        .set('#O', {
+            x: 0,
+            y: 0
+        }, 5.1)
+        .set('#O', {
+            x: 50,
+            y: 150
+        }, 5.4)
+        .set('#O', {
+            x: 0,
+            y: 0
+        }, 5.6);
 
-tl.set('#D', {
-        x: 50,
-        y: -50
-    }, 4.75)
-    .set('#D', {
-        x: 0,
-        y: 0
-    }, 5.1);
+    tl.set('.N', {
+            x: 0,
+            y: -15
+        }, 4.85)
+        .set('.N', {
+            x: 0,
+            y: 0
+        }, 5.2)
+        .set('.N', {
+            rotate: 180
+        }, 5.4)
+        .set('.N', {
+            rotate: 90
+        }, 5.6)
+        .set('.N', {
+            rotate: 0
+        }, 5.8);
+
+    tl.set('#E', {
+            x: -300,
+            y: -120
+        }, 5.5)
+        .set('#E', {
+            x: 0,
+            y: 0
+        }, 5.75);
+
+    tl.set('#D', {
+            x: 50,
+            y: -50
+        }, 4.75)
+        .set('#D', {
+            x: 0,
+            y: 0
+        }, 5.1);
+
+    tl.set('.animation-text--white', {
+        textShadowColor: "rgb(0, 0, 0)"
+    }, 5);
+
+}
