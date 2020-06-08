3600ee3e29656931075614609d38a3e3058c2aea
tangratannakra <stela.tasheva@gmail.com>
2020-06-08 15:16:24 +0300

    fixing relative paths

--- src/scripts/recentProjects/recentPrData.js
+++ src/scripts/recentProjects/recentPrData.js
@@ -1,35 +1,35 @@
 const recentPrData = [{
-        thumb: '../src/img/prjcts/rodina.png',
+        thumb: 'src/img/prjcts/rodina.png',
         title: 'Rodina-Haskovo LTD',
         description: 'Custom Template / Wordpress / Site Admin',
         url: 'http://rodinahaskovo.com/'
     },
     {
-        thumb: '../src/img/prjcts/interlabor.png',
+        thumb: 'src/img/prjcts/interlabor.png',
         title: 'Interlabor',
         description: 'Custom CSS / Custom JavaScript / Site Admin',
         url: 'http://www.inlab-bg.com/'
     },
     {
-        thumb: '../src/img/prjcts/chervenataStena.png',
+        thumb: 'src/img/prjcts/chervenataStena.png',
         title: 'Chervenata Stena Park',
         description: 'UX/UI / WorpdPress / Custom CSS',
         url: 'http://chervenatastena.com/'
     },
     {
-        thumb: '../src/img/prjcts/swissboutique.png',
+        thumb: 'src/img/prjcts/swissboutique.png',
         title: 'Swiss Boutique',
         description: 'JS Customization / Custom CSS / Site Admin',
         url: 'https://swissboutique.bg/'
     },
     {
-        thumb: '../src/img/prjcts/phototravel.png',
+        thumb: 'src/img/prjcts/phototravel.png',
         title: 'PhotoTravel',
         description: 'WordPress and Custom CSS',
         url: 'http://phototravel.bg'
     },
     {
-        thumb: '../src/img/prjcts/realestate.png',
+        thumb: 'src/img/prjcts/realestate.png',
         title: 'RealEstate Agency',
         description: 'CSS / JS / Single Page',
         url: 'https://tangratannakra.github.io/Real-Estate-Agency-Front-Page/'
