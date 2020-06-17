b3c34a432da3d9e783468db17d1955741386f708
tangratannakra <stela.tasheva@gmail.com>
2020-06-17 10:40:30 +0300

    adding section--broken layout and styling it

--- src/scripts/cvData/cvData.js
+++ src/scripts/cvData/cvData.js
@@ -1,19 +1,13 @@
 const cvData = [{
-        title: 'Front-End Developer & Website Manager',
+        title: 'Frontend & Web Developement / Website Management',
         date: '2016 - Present',
-        description: 'Developing and maintaining websites using WordPress / WooCommerce as CMS. Developing WordPress themes using HTML / CSS / JavaScript. Wordpress customization. Creative work for ads. Logo and Brand identity design. Constant self-development through learnig and personal open sourse project with JavaScript and React;'
+        description: 'Write modern, performant, maintainable code for static and dynamic websites on different content management systems (WordPress / WooCommerce / Joomla, etc). Developing WordPress themes and plugins. Website customization and optimization. Creative work for ads. Logo and Brand identity design. Constant self-development through learnig and personal open sourse project with JavaScript and React.'
     },
     {
-        title: 'Web and Graphic designer / Front-End Developer',
+        title: 'UX / UI & Graphic design',
         date: '03 / 2011 – 06 / 2012 @ Web Fashion Ltd',
-        description: 'UX/UI. Creating products and web page designs with Adobe PS/ Adobe Illustrator. Front-End development with HTML/CSS/jQuery'
+        description: 'UX/UI. Creating products and web page designs with Adobe PS/ Adobe Illustrator. Front-End development with HTML / CSS / jQuery'
     }
 ];
 
 export default cvData;
-
-// {
-//     title: 'Software Engineer & Program Manager',
-//     date: '2010 - 2011 @ ASIC Depot',
-//     description: 'writing Perl scripts for JIRA. Designing the work flow of the scripts and their functionality. Testing the scripts. Managing project documentation. Making project plans. Project tracking.'
-// }
